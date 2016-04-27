dd2d0127ce9020558b68307b6a700892faae6584
Maciej Woźniak <iskeld@gmail.com>
2016-04-27 17:05:11 +0200

    Cons update

--- SwiftIntroduction.playground/Pages/cons.xcplaygroundpage/Contents.swift
+++ SwiftIntroduction.playground/Pages/cons.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@
  * Brak wsparcia językowego dla operacji asynchronicznych
  * _Type inference_ mogłoby być lepsze
  * Nieco skomplikowane użycie parametrów generycznych w protokołach
- * _"Kakaowa Egzotyczyność"_ (pewne elementy języka, biblioteki)
+ * _"Kakaowa Egzotyczyność"_ (biblioteki Cocoa, pewne elementy języka)
  ---
  [⬅️⬅️](@previous) [➡️➡️](@next)
  */
